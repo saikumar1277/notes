e02696c586bc16615cf654f8651ff6d9e2f46bbe
sai kumar kongari <saikumar@Mac-11347.lan>
2025-06-22 11:50:58 -0400

    resolved server connection issue

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,8 +1,6 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from '@/lib/trpc/server';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '@/lib/prisma';
 
 const handler = (req: Request) =>
   fetchRequestHandler({
